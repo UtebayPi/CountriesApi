f1f7b8519d7f6dbdfb1cec6291af151837a85c02
UtebayPi <persiutebay@gmail.com>
2022-06-14 15:41:49 +0600

    Немного доработал навигацию.

--- app/src/main/java/com/utebayKazAlm/countriesapi/MainActivity.kt
+++ app/src/main/java/com/utebayKazAlm/countriesapi/MainActivity.kt
@@ -2,15 +2,28 @@ package com.utebayKazAlm.countriesapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.utebayKazAlm.countriesapi.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.NavHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+        //Нужен чтобы при переходах, отображалось кнопка назал сверху экрана.
+        setupActionBarWithNavController(navController)
+    }
+
+    //Чтобы при нажатий кнопки назад сверху, не выходило из activity, а передвигалось по фрагментам.
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
